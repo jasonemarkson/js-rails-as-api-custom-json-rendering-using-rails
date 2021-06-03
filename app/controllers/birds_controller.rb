c02baad9390628389d77ec7bdf59a626f8b591f6
jasonemarkson <jasonmarkson76@gmail.com>
2021-06-02 22:40:38 -0400

    Done.

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,6 +1,19 @@
 class BirdsController < ApplicationController
   def index
-    @birds = Bird.all
-    render json: @birds
+    birds = Bird.all
+    render json: birds, except: [:created_at, :updated_at]
+  end
+
+  def show
+    bird = Bird.find_by(id: params[:id])
+
+    if bird
+      render json: bird, except: [:created_at, :updated_at]
+    else
+      render json: {message: "Bird ain't here bro"}
+    end
+
+    
+    # render json: bird, only: [:id, :name, :species]
   end
 end
